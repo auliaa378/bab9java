7116f30ec1c95b42a7b59bfeb7f918a3c433afa8
auliaa378 <auliajuliana378@gmail.com>
2025-08-05 14:33:02 +0700

    Add files via upload

---
+++ Newprojectbab9.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package newprojectbab9;
+
+/**
+ *
+ * @author PC 22 - LAB R1
+ */
+public class Newprojectbab9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
